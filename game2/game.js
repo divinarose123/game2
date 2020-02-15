3b17ef39f0b545d9f6acb96840204425e3a1ee66
DivinaRose <divinarose123@gmail.com>
2020-02-15 11:03:26 -0800

    add stuff to index.html

--- game2/game.js
+++ game2/game.js
@@ -11,39 +11,41 @@ if(enterHouse == true) {
 alert("tucker and zyra sees three gray balls");
 }
 var chooseBall = prompt("They walk into the main room and found ball. Do you want to try 1, 2, or 3?");
-
+var gameOver = true 
+if(gameOver == false) {
 if(chooseBall == 1) {
     alert("ball bounced just right! you get to the next level!");
+
+}else if(chooseBall == 3) {
+    alert("ohh my favorite ball!. Next level")
+    
+}else {
+    alert("ball deflated. Game over!")
+    gameOver = true
 }
-else if(chooseBall == 3) {
-    alert("ball deflated. Game over!");
-    console.log('GAME OVER')
 }
-else (
-    alert("new ball!. Next level!")
-)
 var enterKitchen = confirm("entering the kitchen")
     alert("BONES, BONES, BONES!")
 var chooseBones = prompt("Large bones, Medium bones, Small bones")
 
 if(chooseBones == "Large bones") {
     alert ("large bone is way too big! game over!");
-    console.log('GAME OVER')
+   
 }
 else if(chooseBones == "Medium bones") {
     alert("the Medium bones is just right..enjoying! onto the next room")
 }
 else { 
     alert("choking! choking! Game over")
-    console.log('GAME OVER')
-}
+}   
+
 //as the day starting to end they both headed upstairs bedroom
 var enterBedroom = confirm("entering the bedroom")
 
 var chooseBed = prompt("They see 3 beds to sleep, left, right, middle")
 if(chooseBed == "left") {
     alert("the bed is deflated. uncomfortable! game over!");
-    console.log('GAME OVER')
+    
 }
 else if(chooseBed == "middle") {
     alert("sleeping in bed with mommy, feel safe and nice. You win!")
@@ -51,5 +53,8 @@ else if(chooseBed == "middle") {
 }
 else {
     alert("tossing and turning. too soft. The End!")
-    console.log('THE END')
+    gameOver = false
+}
+if(gameOver) {
+
 }
