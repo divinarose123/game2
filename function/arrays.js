5ff4668a3f16e7ce59d589164fd0133b4ac38338
DivinaRose <divinarose123@gmail.com>
2020-02-19 23:09:08 -0800

    add stuff to index.html

---
+++ function/arrays.js
@@ -0,0 +1,97 @@
+// ---------------------------
+// The Arrays You Will Work With
+// ---------------------------
+var instructors = [
+    { firstname : 'JD', teaches : 'JavaScript'},
+    { firstname : 'Tim', teaches : 'JavaScript'},
+    { firstname : 'Brit', teaches : 'Ruby'},
+    { firstname : 'Joe', teaches : 'iOS'},
+    { firstname : 'Jake', teaches : 'JavaScript'},
+    { firstname : 'Will', teaches : 'JavaScript'},
+    { firstname : 'Calvin', teaches : 'JavaScript'},
+    { firstname : 'James', teaches : 'Ruby'}
+];
+
+
+// ---------------------------
+// 1. Find largest number
+// ---------------------------
+var largestNumber = [1,12,4,18,9,7,11,3,101,5,6];
+for (var index = 0; index < 10; index++) {
+    largestNumbers = [index]
+    console.log(largestNumber[8])
+
+}
+
+
+// ---------------------------
+// 2. Find longest string
+// ---------------------------
+var Strings = ['this','is','a','collection','of','words'];
+for (var i = 0; i < 5; i++) {
+  Strings = [i]
+  console.log(Strings[3])
+}
+
+// ---------------------------
+// 3. Find even numbers
+// ---------------------------
+var evenNumbers = [1,12,4,18,9,7,11,3,101,5,6];
+for(var index=0; index < evenNumbers.length; index--) {
+    evenNumbers = [index]
+    console.log(evenNumbers[index])
+}
+
+
+// ---------------------------
+// 4. Find odd numbers
+// ---------------------------
+var oddNumbers = [1,12,4,18,9,7,11,3,101,5,6];
+for(var index=0; index < 10; index++) {
+    oddNumbers = [index]
+    console.log(oddNumbers[index])
+}
+
+// ---------------------------
+// 5. Find words that contain `is`
+// ---------------------------
+var Strings = ['this','is','a','collection','of','words'];
+for(var i = 0; i < 5; i++) {
+    Strings = [i]
+  console.log(Strings[1])
+}
+
+
+// ---------------------------
+// 5. Join Both Arrays Together
+// ---------------------------
+var Numbers = [1,12,4,18,9,7,11,3,101,5,6];
+var Strings = ['this','is','a','collection','of','words' ];
+for(var i=0; i < 10; i++) {
+
+  console.log(Numbers[i])
+  console.log(Strings[i])
+}
+
+
+// ---------------------------
+// 6. Use the Instructors array and find all that teach JavaScript, 
+//    then sort them alphabetically
+// ---------------------------
+
+var instructors = [
+  { firstname : 'JD', teaches : 'JavaScript'},
+  { firstname : 'Tim', teaches : 'JavaScript'},
+  { firstname : 'Brit', teaches : 'Ruby'},
+  { firstname : 'Joe', teaches : 'iOS'},
+  { firstname : 'Jake', teaches : 'JavaScript'},
+  { firstname : 'Will', teaches : 'JavaScript'},
+  { firstname : 'Calvin', teaches : 'JavaScript'},
+  { firstname : 'James', teaches : 'Ruby'}
+];
+function sort(javaScript) {
+var javaScript = [JD, Tim, Jake, Will, Calvin]
+
+console.log(instructors)
+
+}
